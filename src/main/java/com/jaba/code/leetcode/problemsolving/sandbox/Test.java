b1627e79c4a729e040ef3291afe29375c7d4cc78
Augusto Baltazar <augusto.jaba@gmail.com>
2020-05-03 17:27:56 +0100

    Sandbox test

---
+++ src/main/java/com/jaba/code/leetcode/problemsolving/sandbox/Test.java
@@ -0,0 +1,9 @@
+package com.jaba.code.leetcode.problemsolving.sandbox;
+
+public class Test {
+
+  public static void main(final String[] args) {
+    final char a = 'a';
+    System.out.println((int) a);
+  }
+}
